ff9f5cd2fa72ced225a4ff3e9ebf2f66836ed56d
David Toledo <dtoledoz0207@gmail.com>
2019-08-21 13:23:15 -0500

    editTaskView-updateMethodTasks-Added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,25 @@
 class TasksController < ApplicationController
+  def edit
+    @todolist = Todolist.find(params[:todolist_id])
+    @task = Task.find(params[:id])
+  end
+
   def create
     @todolist = Todolist.find(params[:todolist_id])
     @task = @todolist.tasks.create(task_params)
     redirect_to todolist_path(@todolist)
   end
 
+  def update
+    @todolist = Todolist.find(params[:todolist_id])
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to todolist_path(@todolist)
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :description)
+    params.require(:task).permit(:title, :description, :status)
   end
 end
