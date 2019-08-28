8c8d1917990ab4ef8a0795f7b8d22abb9faa9bab
David Toledo <dtoledoz0207@gmail.com>
2019-08-28 12:14:39 -0500

    validations-in-taskModel-Added

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :todolist
+  validates :title, :description, presence: true, format: { with: /\A[a-zA-Z0-9 ]+\z/, message: "only allows letters and numbers" }
 
   def self.to_csv(fields = column_names)
     CSV.generate(headers: true) do |csv|
