cd5065bdcf119ecbf4e74ac630e3eb8e1243b64d
David Toledo <dtoledoz0207@gmail.com>
2019-08-28 11:10:51 -0500

    validations-in-todolistModel-Added

--- app/models/todolist.rb
+++ app/models/todolist.rb
@@ -1,4 +1,6 @@
 class Todolist < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
+
+  validates :title, presence: true, format: { with: /\A[a-zA-Z0-9 ]+\z/, message: "only allows letters and numbers" }
 end
